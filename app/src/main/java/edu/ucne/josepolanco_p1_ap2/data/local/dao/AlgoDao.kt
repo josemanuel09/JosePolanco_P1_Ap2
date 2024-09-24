261f16bc30d53b5657a86fc6b4fcf7773fbd4ea7
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-09-24 13:48:04 -0400

    Agrega el Dao

---
+++ app/src/main/java/edu/ucne/josepolanco_p1_ap2/data/local/dao/AlgoDao.kt
@@ -0,0 +1,23 @@
+package edu.ucne.josepolanco_p1_ap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.josepolanco_p1_ap2.data.local.entity.AlgoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlgoDao {
+    @Upsert
+    suspend fun save(algo: AlgoEntity)
+
+    @Query("Select * from Algo where algoId = :id")
+    suspend fun find(id: Int): AlgoEntity
+
+    @Delete
+    suspend fun delete(algo: AlgoEntity)
+
+    @Query("Select * from Algo")
+    suspend fun getAll(): Flow<List<AlgoEntity>>
+}
